f0d8cef19eacca51e9dbd10ec91b4fd6448b5665
Michal Slawinski <michal.slawin@gmail.com>
2021-02-22 10:48:45 +0100

    following balls spawner

---
+++ Assets/Ball2d/Scripts/DeathSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject deathBallPrefab = null;
+    [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private float followTime = 3f;
+    private PlayerController player = null;
+    private GameObject spawnedBall = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        if (player == null) throw new System.Exception("Player not found");
+
+        SpawnBall();
+
+        Debug.Log(player.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnedBall != null)
+        {
+            spawnedBall.transform.position = Vector2.MoveTowards(spawnedBall.transform.position, player.transform.position, ballSpeed * Time.deltaTime);
+        }
+        else
+        {
+            SpawnBall();
+        }
+    }
+
+    private void SpawnBall()
+    {
+        spawnedBall = Instantiate(deathBallPrefab, transform.position, Quaternion.identity);
+
+        Destroy(spawnedBall, followTime);
+    }
+}
